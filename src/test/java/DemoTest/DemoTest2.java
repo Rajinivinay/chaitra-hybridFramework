9b61668781cf847ab4c895aebe52127e4fb3b168
Rajinivinay <rajinisingampalli21@gmail.com>
2022-11-18 22:33:22 -0800

    commit 2 demotest

--- src/test/java/DemoTest/DemoTest2.java
+++ src/test/java/DemoTest/DemoTest2.java
@@ -1,10 +1,9 @@
 package DemoTest;
 
-public class demoTest {
+public class DemoTest2 {
 	public static void main(String[] args) {
 		System.out.println("this is my framework");
 		System.out.println("this is my hybrid framework");
-		
 	}
 
 }
