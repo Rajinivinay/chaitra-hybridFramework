b9668840fadad0e7d8e4b429fa17420faab21d27
Rajinivinay <rajinisingampalli21@gmail.com>
2022-11-21 21:55:15 -0800

    TC_Contactsmodule

--- src/test/java/DemoTest/DemoTest2.java
+++ src/test/java/DemoTest/DemoTest2.java
@@ -4,6 +4,11 @@ public class DemoTest2 {
 	public static void main(String[] args) {
 		System.out.println("this is my framework");
 		System.out.println("this is my hybrid framework");
+		
+		
+		System.out.println("hi");
+		
+	
 	}
 
 }
